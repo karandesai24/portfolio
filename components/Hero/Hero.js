aba3f312cbaea2430bd2d8fae8620e9545d1fbed
karandesai24 <karan181716@gmail.com>
2022-11-07 21:45:35 +0530

    commit

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -36,7 +36,7 @@ const Hero = () => {
       );
 
     return () => typed.destroy();
-  }, [typedEl, targetSection, options]);
+  }, [typedEl, targetSection]);
 
   // useEffect(() => {
   //   lottie.loadAnimation({
